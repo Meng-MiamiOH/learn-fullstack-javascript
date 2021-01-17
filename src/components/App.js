b8e2801ff1b502c01f04d75dce3619e8a54a79a3
qum <qum@miamioh.edu>
2021-01-16 19:55:29 -0500

    axios read data

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import Header from './Header';
+import axios from 'axios';
 import ContestPreview from './ContestPreview';
+
 
 class App extends React.Component {
     state = {
-        pageHeader: 'Naming Contests'
+        pageHeader: 'Naming Contests',
+        contests : []
     };
+    componentDidMount(){
+        // ajax ...
+        axios.get('/api/contests')
+        .then(resp=>{
+            this.setState({
+                contests: resp.data.contests
+            });
+        })
+        .catch(console.error);
+
+
+    }
+    componentWillUnmount(){
+
+    }
 
     render() {
         return (
             <div className="App">
                 <Header message={this.state.pageHeader} />
                 <div>
-                    {this.props.contests.map(contest =>
-                        <ContestPreview {...contest} />
+                    {this.state.contests.map(contest =>
+                        <ContestPreview key={contest.id}{...contest} />
                     )}
                 </div>
             </div>
